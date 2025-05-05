17f00db5428f32ccc463a80a0dc062ad96cd1703
Rylan Davidson <RylanDavidson3@gmail.com>
2025-05-05 13:26:36 -0500

    Build Commit

--- Assets/Scripts/TutorialManager.cs
+++ Assets/Scripts/TutorialManager.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using PanettoneGames.GenEvents;
 using System;
-using UnityEditor.EditorTools;
+// using UnityEditor.EditorTools;
 
 public class TutorialManager : MonoBehaviour, IGameEventListener<int>
 {
